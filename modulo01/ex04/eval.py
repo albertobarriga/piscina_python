9f50fb60158a452a9475f70794f612e54a4b7ae6
abarriga <abarriga4242@gmail.com>
2023-03-17 11:58:22 +0100

    ex04 completo

--- modulo01/ex04/eval.py
+++ modulo01/ex04/eval.py
@@ -4,14 +4,26 @@ class Evaluator:
 	def zip_evaluate(coefs, words):
 		if not len(coefs) == len(words):
 			return (-1)
-		tu = []
+		lst = []
 		a = 0
-		tu = list(zip(words, coefs))
-		for i in tu:
-			a += (len(tu[i][0]) * tu[i][1])
-		print (a)
-		
+		lst = list(zip(words, coefs))
+		for i in lst:
+			a += (len(i[0]) * i[1])
+		return (a)
 
-	# @staticmethod
-	# def enumarate_value(coefs, words):
-	# zip_evaluate([1,2], ["hola", "adios"])
+	@staticmethod
+	def enumerate_evaluate(coefs, words):
+		
+		if not len(coefs) == len(words):
+			return (-1)
+		lst = []
+		lst = list(enumerate(words))
+		a = 0
+		for i in lst:
+			a += (len(i[1]) * coefs[i[0]])
+		return(a)
+
+
+words = ["Le", "Lorem", "Ipsum", "est", "simple"]
+coefs = [1.0, 2.0, 1.0, 4.0, 0.5]
+print(Evaluator.zip_evaluate(coefs, words))
