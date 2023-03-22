e0ab58561a34f079ff6f74f42f1401007afbe58e
abarriga <abarriga4242@gmail.com>
2023-03-22 12:22:26 +0100

    dia07

--- modulo02/ex03/csvreader.py
+++ modulo02/ex03/csvreader.py
@@ -6,7 +6,7 @@
 #    By: abarriga <abarriga@student.42malaga.com    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/03/21 11:03:31 by abarriga          #+#    #+#              #
-#    Updated: 2023/03/21 12:44:30 by abarriga         ###   ########.fr        #
+#    Updated: 2023/03/22 12:21:58 by abarriga         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,20 +21,48 @@ class CsvReader():
 		
 	def __enter__(self):
 		self.file = open(self.filename, "r")
+		return self
 
 	def __exit__(self, ):
 		self.file.close()
 		
+
+	def importfile(self):
+		lst = []
+		items = -1
+		lines = self.file.read().split(self.sep)
+		for line in lines:
+			values = line.split(self.sep)
+			if (items == -1):
+				items = len(values)
+			if (items != len(values)):
+				return None
+			lst.append(values)
+		return lst
+		
 	def getdata(self):
 	""" Retrieves the data/records from skip_top to skip bottom.
 	Return:
-	nested list (list(list, list, ...)) representing the data.
+		nested list (list(list, list, ...)) representing the data.
 	"""
-	# ... Your code here ...
-	
+	lst = self.importfile()
+	if (lst == None):
+		return None
+	if self.header:
+		lst = lst[1:]
+	lst = lst[self.skip_top:len(lst) - self.skip_bottom]
+	return (lst)
+
 	def getheader(self):
 	""" Retrieves the header from csv file.
 	Returns:
-	list: representing the data (when self.header is True).
-	None: (when self.header is False).
+		list: representing the data (when self.header is True).
+		None: (when self.header is False).
 	"""
+	lst = self.importfile()
+	if (lst == None):
+		return None
+	if self.header == True:
+		return(lst[0])
+	else:
+		return None
